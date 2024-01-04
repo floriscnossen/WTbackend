bdb8e756c52b58aae61c36e8aaad3697ae5381e9
floriscnossen <floriscnossen@hotmail.com>
2024-01-04 12:12:32 +0100

    Added User class

---
+++ src/main/java/nl/workingtalent/backend/entity/User.java
@@ -0,0 +1,33 @@
+package nl.workingtalent.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
