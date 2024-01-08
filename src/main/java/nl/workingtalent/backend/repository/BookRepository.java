8b909fe47e0e29ef3063c192c6ef8becc684d54f
Abel Jonen <abeljonen@mail.com>
2024-01-08 15:22:35 +0100

    Added services & repositories for Book

---
+++ src/main/java/nl/workingtalent/backend/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package nl.workingtalent.backend.repository;
+
+import nl.workingtalent.backend.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+}
