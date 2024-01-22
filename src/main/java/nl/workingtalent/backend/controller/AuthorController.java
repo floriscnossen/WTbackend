7022b7ca771e1f69470baeb099becb4ce08346c8
cvoogt2000 <cvoogt2000@gmail.com>
2024-01-22 12:01:00 +0100

    Added CrossOrigin to all controllers

--- src/main/java/nl/workingtalent/backend/controller/AuthorController.java
+++ src/main/java/nl/workingtalent/backend/controller/AuthorController.java
@@ -17,7 +17,7 @@ import nl.workingtalent.backend.service.AuthorService;
 
 @RestController
 @RequestMapping(path = "author")
-@CrossOrigin(maxAge = 3600)
+@CrossOrigin
 public class AuthorController {
 	@Autowired
 	AuthorService as;
--- src/main/java/nl/workingtalent/backend/controller/BookController.java
+++ src/main/java/nl/workingtalent/backend/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import nl.workingtalent.backend.service.BookService;
 
 @RestController
 @RequestMapping(path = "product/book")
+@CrossOrigin
 public class BookController {
 	@Autowired
 	BookService bs;
